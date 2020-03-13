be65fc5f2fb2ae69be6d3717d721d5f937dbe266
asthik <asthikgh@gmail.com>
2020-03-13 17:57:55 +0530

    pageload and field values set

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Container from '../src/components/Container/Container';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <Container />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    updateValues : []
+  }
+
+  updateValues(values){
+    this.setState({values});
+  }
+
+  componentDidMount(){
+    axios.get('http://54.193.89.54:8230/readFields')
+    .then((response)=> {
+        this.updateValues( response.data.data);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Container />
+      </div>
+    );
+  }
+
 }
 
 export default App;
--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import './Container.css';
+import FormInput from '../FormInput/FormInput';
+import axios from 'axios';
 
 class Container extends React.Component {
+    state = {
+        firstname : "",
+        lastname : "",
+        email : "",
+        title : "",
+        companyname : "",
+        joblevel : "",
+        jobfunction : "",
+        companysize : "",
+        industry : "",
+        resource : "",
+        streetAddress : "",
+        city : "",
+        state : "",
+        country : "",
+        zipcode : ""
+        // counters: [
+        //     {id:1, value:4},
+        //     {id:2, value:2},
+        //     {id:3, value:0},
+        //     {id:4, value:3}
+        //    ]
+    }
+    
+    handleSubmit = async e => {
+        e.preventDefault();
 
-    oncall = () => {
         (async () => {
             let resp = await fetch('http://54.193.89.54:8230/readFields');
             console.log(resp);
@@ -16,15 +43,171 @@ class Container extends React.Component {
             }
         })();
 
-        // fetch('https://jsonplaceholder.typicode.com/posts/1/comments').then(response => response.json())
-        //     .then(data => console.log(data));
+        // axios({
+        //     url: 'http://54.193.89.54:8230/saveFields',
+        //     method: 'post',
+        //     // data :{"data" : {"name" : this.state.name}}
+        //     data :{"data" : [
+        //         {"id":null,"createdOn": new Date(), "fieldName":this.state.firstname,"fieldType":"TXT","mandatory":"Y","order":1,"expectedValues":[]},
+        //         {"id":null,"createdOn": new Date(), "fieldName":this.state.email,"fieldType":"TXT","mandatory":"Y","order":2,"expectedValues":[]},
+        //         {"id":null,"createdOn": new Date(), "fieldName":this.state.companyname,"fieldType":"TXT","mandatory":"Y","order":3,"expectedValues":[]},
+        //         {"id":null,"createdOn": new Date(), "fieldName":this.state.jobfunction,"fieldType":"TXT","mandatory":"Y","order":4,"expectedValues":[]},
+        //         {"id":null,"createdOn": new Date(), "fieldName":this.state.industry,"fieldType":"TXT","mandatory":"Y","order":5,"expectedValues":[]},
+        //         {"id":null,"createdOn": new Date(), "fieldName":this.state.streetAddress,"fieldType":"TXT","mandatory":"Y","order":6,"expectedValues":[]},
+        //         {"id":null,"createdOn": new Date(), "fieldName":this.state.state,"fieldType":"TXT","mandatory":"Y","order":7,"expectedValues":[]},
+        //         {"id":null,"createdOn": new Date(), "fieldName":this.state.zipcode,"fieldType":"TXT","mandatory":"Y","order":8,"expectedValues":[]},
+        //         {"id":null,"createdOn": new Date(), "fieldName":this.state.lastname,"fieldType":"TXT","mandatory":"Y","order":9,"expectedValues":[]},
+        //         {"id":null,"createdOn": new Date(), "fieldName":this.state.title,"fieldType":"TXT","mandatory":"Y","order":10,"expectedValues":[]},
+        //         {"id":null,"createdOn": new Date(), "fieldName":this.state.joblevel,"fieldType":"TXT","mandatory":"Y","order":11,"expectedValues":[]},
+        //         {"id":null,"createdOn": new Date(), "fieldName":this.state.companysize,"fieldType":"TXT","mandatory":"Y","order":12,"expectedValues":[]},
+        //         {"id":null,"createdOn": new Date(), "fieldName":this.state.resource,"fieldType":"TXT","mandatory":"Y","order":13,"expectedValues":[]},
+        //         {"id":null,"createdOn": new Date(), "fieldName":this.state.city,"fieldType":"TXT","mandatory":"Y","order":14,"expectedValues":[]},
+        //         {"id":null,"createdOn": new Date(), "fieldName":this.state.country,"fieldType":"TXT","mandatory":"Y","order":15,"expectedValues":[]}
+        //     ]
+        //     }
+        //   }).then((response) => {
+        //     if(response.data.status === "success"){
+        //         // this.props.updateValues({values : response.data.data}) ;
+        //         axios.get('http://54.193.89.54:8230/readFields')
+        //         .then((response)=> {
+        //             this.props.updateValues(response.data.data) ;
+        //             this.setState({errorMsg: ""});
+        //             this.handleReset();
+        //         });
+        //     }else{
+        //         this.setState({errorMsg: response.data.message});
+        //     }
+        // });
+    }
+
+    handleChange = (e) => {
+        const { value, name } = e.target;
+        this.setState({[name] : value });
     }
 
     render(){
         return (
             <div className="container">
-              container works!!
-              <input type="button" onClick={this.oncall} value="Save" />
+              {/* <input type="text" class="col-md-3 col-lg-6"/> */}
+
+              <form onSubmit={this.handleSubmit}>
+
+              {/* {this.state.counters.map((e,i) => 
+                <FormInput key={e.id} onDelete={this.handleDelete} counter={e} selected ...otherProps/>               
+                )} */}
+                <div className="leftContainer col-md-6">
+                    <FormInput 
+                        type="text" 
+                        value={this.state.firstname} 
+                        label="First Name"
+                        name="firstname" 
+                        required 
+                        handleChange={this.handleChange}/>
+                    <FormInput 
+                        type="text" 
+                        value={this.state.email} 
+                        label="Email Address"
+                        name="email" 
+                        required 
+                        handleChange={this.handleChange}/>
+                    <FormInput 
+                        type="text" 
+                        value={this.state.companyname} 
+                        label="Company Name"
+                        name="companyname" 
+                        required 
+                        handleChange={this.handleChange}/>
+                    <FormInput 
+                        type="text" 
+                        value={this.state.jobfunction} 
+                        label="Job function"
+                        name="jobfunction" 
+                        required 
+                        handleChange={this.handleChange}/>
+                    <FormInput 
+                        type="text" 
+                        value={this.state.industry} 
+                        label="Industry"
+                        name="industry" 
+                        required 
+                        handleChange={this.handleChange}/>
+                    <FormInput 
+                        type="text" 
+                        value={this.state.streetAddress} 
+                        label="Street Address"
+                        name="streetAddress" 
+                        required 
+                        handleChange={this.handleChange}/>
+                    <FormInput 
+                        type="text" 
+                        value={this.state.state} 
+                        label="State"
+                        name="state" 
+                        required 
+                        handleChange={this.handleChange}/>
+                    <FormInput 
+                        type="text" 
+                        value={this.state.zipcode} 
+                        label="Zip code"
+                        name="zipcode" 
+                        required 
+                        handleChange={this.handleChange}/>
+                </div>
+                <div className="rightContainer col-md-6">
+                    <FormInput 
+                        type="text" 
+                        value={this.state.lastname} 
+                        label="Last Name"
+                        name="lastname" 
+                        required 
+                        handleChange={this.handleChange}/>
+                    <FormInput 
+                        type="text" 
+                        value={this.state.title} 
+                        label="Title"
+                        name="title" 
+                        required 
+                        handleChange={this.handleChange}/>
+                    <FormInput 
+                        type="text" 
+                        value={this.state.firstname} 
+                        label="Job Level"
+                        name="joblevel" 
+                        required 
+                        handleChange={this.handleChange}/>
+                    <FormInput 
+                        type="text" 
+                        value={this.state.companysize} 
+                        label="Company Size"
+                        name="companysize" 
+                        required 
+                        handleChange={this.handleChange}/>
+                    <FormInput 
+                        type="text" 
+                        value={this.state.resource} 
+                        label="Resource"
+                        name="resource" 
+                        required 
+                        handleChange={this.handleChange}/>
+                    <FormInput 
+                        type="text" 
+                        value={this.state.city} 
+                        label="City"
+                        name="city" 
+                        required 
+                        handleChange={this.handleChange}/>
+                    <FormInput 
+                        type="text" 
+                        value={this.state.country} 
+                        label="Country"
+                        name="country" 
+                        required 
+                        handleChange={this.handleChange}/>
+                </div>
+
+              <input type="submit" value="Save" />
+                    
+                </form>
             </div>
           );
     }
